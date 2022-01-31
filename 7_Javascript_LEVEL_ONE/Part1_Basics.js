38f90c8e7b7ca1210a032949ffca325db0358ac1
wrafalit@gmail.com <Programowanie2020#>
2022-01-31 19:08:16 +0100

    js Basics command

---
+++ 7_Javascript_LEVEL_ONE/Part1_Basics.js
@@ -0,0 +1,32 @@
+"Django"
+
+"Django".length
+6
+
+"x ".length
+2
+
+"hello"[0]
+'h'
+
+var bankAccount = 100;
+
+bankAccount
+100
+
+var greeting = "Welcome back: "
+
+var name = "Jose";
+
+alert(greeting+name)
+
+console.log("Hey O'm in the console");
+VM1521:1 Hey O'm in the console
+
+prompt('Enter something')
+'sad'
+
+var age = prompt('Enter your age'); 35
+
+age
+35
