e29a907c09fa39a5d5b80faaec13cca57e9dc35c
wrafalit@gmail.com <Programowanie2020#>
2022-02-02 20:50:11 +0100

    while

---
+++ 7_Javascript_LEVEL_ONE/Part6_while.js
@@ -0,0 +1,22 @@
+// var x = 0;
+//
+// while (x < 5) {
+//   console.log("x is currently: " +x);
+//
+//   if (x===3) {
+//     console.log("X is equal THREE!");
+//     break;
+//   }
+//
+//   console.log('x is still less than 5, adding 1 to x');
+//   x += 1;
+// }
+
+var x = 1;
+
+while (x <= 10) {
+  if (x % 2 === 0) {
+    console.log(x);
+  }
+  x += 1;
+}
