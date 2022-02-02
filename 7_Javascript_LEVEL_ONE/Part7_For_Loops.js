508e96ec8c2a3897996bcde2823f0b2f9771a7ae
wrafalit@gmail.com <Programowanie2020#>
2022-02-02 21:00:53 +0100

    for loop

---
+++ 7_Javascript_LEVEL_ONE/Part7_For_Loops.js
@@ -0,0 +1,13 @@
+// for (var i = 0; i < 5; i++) {
+//   console.log("Number is " +i);
+// }
+
+// var word = "ABCDEFGIJK"
+// for (var i = 0; i < word.length; i++) {
+//   console.log(word[i]);
+// }
+
+var word = "ABABABABAB"
+for (var i = 0; i < word.length; i = i + 2) {
+  console.log(word[i]);
+}
